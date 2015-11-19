f24eb8ff3d6a1e67396e98e8ad93ad4e96abbd72
aparracorbacho <aparracorbacho@atanasoff20.danielcastelao.org>
2015-11-19 09:58:16 +0100

    Boletin 10 Ejercicio 1

--- Boletin 10 Ejercicio1/src/boletin/pkg10/ejercicio1/Boletin10Ejercicio1.java
+++ Boletin 10 Ejercicio1/src/boletin/pkg10/ejercicio1/Boletin10Ejercicio1.java
@@ -6,7 +6,6 @@
 package boletin.pkg10.ejercicio1;
 
 import java.util.Scanner;
-import javax.swing.JOptionPane;
 
 /**
  *
@@ -21,18 +20,24 @@ public class Boletin10Ejercicio1 {
         // TODO code application logic here
         String repetirp;
         do {
-        System.out.println("Vamos a jugar a un juego\nIntroduce primero el numero a adiviniar");
-        juegoCalc obx = new juegoCalc();
-        obx.pedir();
-        obx.jugar();
-        System.out.println("Quieres volver a jugar?\nResponde 'si' o 'no'");
-        Scanner repetirpTeclado = new Scanner(System.in);
-        repetirp = repetirpTeclado.next();
-        if (repetirp.equals("no")) {
-            System.out.println("-----------------------------------\nGracias por jugar\n-----------------------------------");
-        } else if ((!repetirp.equals("si")) || (!repetirp.equals("no"))) {
-        }
+            System.out.println("Vamos a jugar a un juego\nIntroduce primero el numero a adiviniar");
+            juegoCalc obx = new juegoCalc();
+            obx.pedir();
+            obx.jugaracom();
+            int repetirm = 0;
+            do {
+            System.out.println("Quieres volver a jugar?\nResponde 'si' o 'no'");
+            Scanner repetirpTeclado = new Scanner(System.in);
+            repetirp = repetirpTeclado.next();
+            if (repetirp.equals("no")) {
+                System.out.println("-----------------------------------\nGracias por jugar\n-----------------------------------");
+                repetirm = 0;
+            } else if ((!repetirp.equals("si")) || (!repetirp.equals("no"))) {
+                System.out.println("No es una respuesta valida, responde 'si' o 'no'");
+                repetirm = 1;
+            }
+            } while (repetirm == 1);
         } while (repetirp.equals("si"));
     }
-    
+
 }
